5fe11a3faca074425628a3b3466b4591f101eac0
SageSG <houliangyip@gmail.com>
2017-07-11 13:18:45 +0800

    added copy paste code

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ var gateway = braintree.connect({
   publicKey: "ymkd4bvhhwrg48fz",
   privateKey: "0c913707bb92caa67f77b31dca2fcf4a"
 });
+function requestnonce (){
+    app.get("/client_token", function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.send(response.clientToken);
+  });
+});
+}
+function getnonce(){
+    app.post("/checkout", function (req, res) {
+  var nonceFromTheClient = req.body.payment_method_nonce;
+  // Use payment method nonce here
+});
+}
+function chargecard (nonceFromTheClient){
+gateway.transaction.sale({
+  amount: "10.00",
+  paymentMethodNonce: nonceFromTheClient,
+  options: {
+    submitForSettlement: true
+  }
+}, function (err, result) {
+});
+}
