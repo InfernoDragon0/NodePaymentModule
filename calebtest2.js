8b314b519c86e113b1c3dae27ef4ef31e9e3bc9c
carleb <calebcheongdnt@gmail.com>
2017-07-19 12:03:54 +0800

    cosmosDB test2 v1.3

--- calebtest2.js
+++ calebtest2.js
@@ -1,7 +1,7 @@
 var cosmosConfig = require("./calebtest2config");
 var docdbClient = require("documentdb").DocumentClient;
 
-var client = new docdbClient(cosmosConfig.uri,{masterKey: cosmosConfig.primaryKey});
+var client = new docdbClient(cosmosConfig.endpoint,{masterKey: cosmosConfig.primaryKey});
 
 var HttpStatusCodes = { NOTFOUND: 404 }
 
@@ -9,7 +9,8 @@ var HttpStatusCodes = { NOTFOUND: 404 }
 var databaseUrl =`dbs/${cosmosConfig.database.id}`;
 var collectionUrl = `${databaseUrl}/colls/${cosmosConfig.collection.id}`;
 console.log (databaseUrl);
-
+ 
+ var testUrl = "dbs/jElement";
 
 function createDbIfNotExists(){
     client.readDatabase(databaseUrl, (err, result) => {
@@ -29,4 +30,26 @@ function createDbIfNotExists(){
         }
     });
 };
-createDbIfNotExists();
+
+console.log(testUrl);
+//createDbIfNotExists();
+
+function createCollectionIfNotExists(){
+    client.readCollection(collectionUrl, (err, result) => {
+        if(err){
+            client.createCollection(databaseUrl, cosmosConfig.collection, null, (err, created) => {
+                    if(err){
+                        console.log(JSON.stringify(err));
+                        
+                    }
+                    else{
+                        console.log(JSON.stringify(created));
+                    }
+            });
+        }
+        else{
+            console.log(JSON.stringify(result));
+        }
+    });
+};
+createCollectionIfNotExists();
