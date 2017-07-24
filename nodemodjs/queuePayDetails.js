7601a3076b2d621a00b74f3ad185a553f19c3be4
carleb <calebcheongdnt@gmail.com>
2017-07-24 17:18:34 +0800

    update azure storage link

--- nodemodjs/queuePayDetails.js
+++ nodemodjs/queuePayDetails.js
@@ -2,8 +2,9 @@ const storageAzure = require('azure-storage')
 // const retryOperations = new azure.ExponentialRetryPolicyFilter();
 // const tblSvc = azure.createTableService(config.tblAccountName, config.tblAccessKey1).withFilter(retryOperations);
 
-//let AzureWebJobsStorage = 'DefaultEndpointsProtocol=https;AccountName=jiraffeteststorage;AccountKey=aojV/ZUm2XJWgE31TYp4SK4igk5/6UVCjn+fagDG0Hr0BemXs9PipCJy5Sca+VwpaT7eYLozaCDL9YHyCdq9AA==;EndpointSuffix=core.windows.net'
-let AzureWebJobsStorage = 'DefaultEndpointsProtocol=https;AccountName=testqueuecaleb;AccountKey=1zBghaTPU9cjp5J8Ea/LSGp8B5EtUWPnL0V6P+8JnW60eCkuZhNGCoZ6/rEeXneVVOFVnwKiwS/FUtd1b6f+vw==;EndpointSuffix=core.windows.net'
+let AzureWebJobsStorage = 'DefaultEndpointsProtocol=https;AccountName=jiraffeteststorage;AccountKey=aojV/ZUm2XJWgE31TYp4SK4igk5/6UVCjn+fagDG0Hr0BemXs9PipCJy5Sca+VwpaT7eYLozaCDL9YHyCdq9AA==;EndpointSuffix=core.windows.net'
+// test link to caleb azure storage , dele in finnal
+// let AzureWebJobsStorage = 'DefaultEndpointsProtocol=https;AccountName=testqueuecaleb;AccountKey=1zBghaTPU9cjp5J8Ea/LSGp8B5EtUWPnL0V6P+8JnW60eCkuZhNGCoZ6/rEeXneVVOFVnwKiwS/FUtd1b6f+vw==;EndpointSuffix=core.windows.net'
 // let successMessage = { address: 'get address from bot', type: 'PAY_SUCCESS', text: 'send necessary details' };
 // let failureMessage = { address: 'get address from bot', type: 'PAY_FAILURE', text: 'send necessary details' }; // json text : card details , amount
 
