627effab7744b8b46a4d28b8bfd0353f1967a36d
Unknown <calebcheongdnt@gmail.com>
2018-03-19 22:04:47 +0800

    fix

--- nodemodjs/config/databaseConfig.js
+++ nodemodjs/config/databaseConfig.js
@@ -1,6 +1,6 @@
 var databaseConfig = {}
 
-databaseConfig.url = 'http://b40fb310.ngrok.io/api'
+databaseConfig.url = 'http://johorbahru.azurewebsites.net/api'
 
 databaseConfig.primary_key = 'NnGUnatosykldCDs6m5Ma4tBGlb6Wyue912JLQ=='
 
